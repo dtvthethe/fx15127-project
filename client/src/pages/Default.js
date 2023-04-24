cd1574fe8f0f244a2698646ba2bd5ce01ff1043b
dtvthethe <dtvthe@gmail.com>
2023-04-25 03:27:40 +0700

    meta-mask-authen

--- client/src/pages/Default.js
+++ client/src/pages/Default.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Web3 from 'web3';
+import { Gitlab } from 'react-feather';
 
 export default function Default() {
+  const loginMetaMask = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+  }
+
+  // useEffect(() => {
+  //   loginMetaMask();
+  // }, [])
+
   return (
     <>
-        Default page
+        <button className='btn btn-sm btn-warring' onClick={() => loginMetaMask()}>
+          <Gitlab width={18} height={18} className='mt--3' />
+          Login with MetaMask
+        </button>
     </>
   );
 }
