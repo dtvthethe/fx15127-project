c25f752d998ad02e175c79ea77f5f0553cb3b318
dtvthethe <dtvthe@gmail.com>
2023-05-24 00:08:15 +0700

    add Todo

---
+++ client/src/middleware/UserMiddleware.js
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { getCurrentAccountLogined } from "../common/MetaMask";
+
+export default function AuthMiddleware({ children }) {
+  const currentAccount = getCurrentAccountLogined();
+  
+  if (currentAccount && currentAccount.data && currentAccount.data != null) {
+    return children;
+  } else {
+    return <Navigate to="/" />
+  }
+}
